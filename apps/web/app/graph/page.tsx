4b3aec4aca8fd94270c311a7b3e3daeab166998c
zaralynx6-coder <zaralynx6@gmail.com>
2026-04-02 18:05:53 -0300

    Fix broken monorepo configs and restore app/runtime files

--- apps/web/app/graph/page.tsx
+++ apps/web/app/graph/page.tsx
@@ -1,15 +1,5 @@
-import GraphExplorer from "../../components/GraphExplorer";
+import GraphExplorer from '../../components/GraphExplorer';
 
 export default function GraphPage() {
   return <GraphExplorer />;
-import { GraphView } from '../../components/GraphView';
-
-export default function GraphPage() {
-  return (
-    <main>
-      <h1>Grafo Empresarial</h1>
-      <p>Clique em um nó para expandir conexões.</p>
-      <GraphView />
-    </main>
-  );
 }
